7c75db0ab606c4c7ae31d010b202efacd483f984
jojo3416 <jojo3416jo@gmail.com>
2026-01-28 20:08:06 +0530

    updated homework

--- Tutorial/Misc/test2.js
+++ Tutorial/Misc/test2.js
@@ -1,14 +1,34 @@
-function text_clean (txt) {
-tr = txt.trim();    
-lcase = tr.toLowerCase();
-rep = lcase.replace("food","ambience");
-pos = rep.indexOf("service");
-spli = rep.split(" ");
 
 
-console.log("Output " + spli + "\n" + " position of service " + pos)
+function cleanUserBio(profile) {
+    username = profile.username;
+    bio = profile.bio;
+
+    tr_bio = bio.trim();    
+    lcase_bio = tr_bio.toLowerCase();
+    rep_bio = lcase_bio.replace("javascript","js"); 
+    count = rep_bio.length;
+
+    tr_usr = username.trim();    
+    lcase_usr = tr_usr.toLowerCase();
+    rep_usr = lcase_usr.replace("javascript","js"); 
+    
+
+    return [rep_usr, rep_bio];
+
 }
 
-var feedback = "   I loved the FOOD and the service!   ";
+function usrbio(name,bio) {
+    len_bio = bio.length;
+    console.log(`\n User Name : ${name}\n Bio : ${bio}\n Length of bio : ${len_bio} `)
 
-text_clean(feedback);
+}
+
+const userProfile = {
+    username : "CoolUser99",
+    bio : " I LOVE coding! JavaScript is Great!!! "
+}
+
+const [usr_name, usr_bio] = cleanUserBio(userProfile);
+
+usrbio(usr_name,usr_bio);
