c697d0e7bbd5c7b67fb5a8e1f6048f56348f2311
jojo3416 <jojo3416jo@gmail.com>
2026-01-29 19:21:27 +0530

    updated classwork

--- Tutorial/Misc/test2.js
+++ Tutorial/Misc/test2.js
@@ -1,34 +1,13 @@
 
 
-function cleanUserBio(profile) {
-    username = profile.username;
-    bio = profile.bio;
 
-    tr_bio = bio.trim();    
-    lcase_bio = tr_bio.toLowerCase();
-    rep_bio = lcase_bio.replace("javascript","js"); 
-    count = rep_bio.length;
-
-    tr_usr = username.trim();    
-    lcase_usr = tr_usr.toLowerCase();
-    rep_usr = lcase_usr.replace("javascript","js"); 
-    
-
-    return [rep_usr, rep_bio];
-
-}
-
-function usrbio(name,bio) {
-    len_bio = bio.length;
-    console.log(`\n User Name : ${name}\n Bio : ${bio}\n Length of bio : ${len_bio} `)
-
-}
-
-const userProfile = {
-    username : "CoolUser99",
-    bio : " I LOVE coding! JavaScript is Great!!! "
-}
-
-const [usr_name, usr_bio] = cleanUserBio(userProfile);
-
-usrbio(usr_name,usr_bio);
+const random = Math.floor(Math.random() * 100) ;
+const score = Number("75");
+const diff = score - random ;Math.floor(Math.random() * 100) ;
+const roundsqrt = Math.round(Math.sqrt(score));
+const pivalue = Math.PI ;
+const implicit = "50" * 2;
+console.log(score + " - " + random + " = " + diff);
+console.log("rounded sqrt " + roundsqrt);
+console.log("pi value " + pivalue);
+console.log("'50' * 2 " + implicit );
