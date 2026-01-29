982207de1c24c2841857aa3f7f705588839f2d09
jojo3416 <jojo3416jo@gmail.com>
2026-01-29 20:08:57 +0530

    updated homework

--- Tutorial/Misc/test2.js
+++ Tutorial/Misc/test2.js
@@ -1,13 +1,29 @@
 
 
+const mysteryLevel = Math.floor(Math.random() * 50) + 1;
 
-const random = Math.floor(Math.random() * 100) ;
-const score = Number("75");
-const diff = score - random ;Math.floor(Math.random() * 100) ;
-const roundsqrt = Math.round(Math.sqrt(score));
-const pivalue = Math.PI ;
-const implicit = "50" * 2;
-console.log(score + " - " + random + " = " + diff);
-console.log("rounded sqrt " + roundsqrt);
-console.log("pi value " + pivalue);
-console.log("'50' * 2 " + implicit );
+
+const userInput = "45";
+const userScore = Number(userInput); 
+
+const diff = Math.abs(mysteryLevel - userScore);
+
+const scoreSqrt = Math.sqrt(userScore);
+
+
+const piValue = Math.PI;
+
+const implicitResult = "20" * 5;
+
+
+const highest = Math.max(10, 20, mysteryLevel);
+const lowest = Math.min(10, 20, mysteryLevel);
+
+
+console.log(`Mystery Level: ${mysteryLevel}`);
+console.log(`User Score: ${userScore}`);
+console.log(`Difference: ${diff}`);
+console.log(`Square Root of Score: ${scoreSqrt}`);
+console.log(`Value of Pi: ${piValue}`);
+console.log(`Implicit (20 * 5): ${implicitResult}`);
+console.log(`Highest value: ${highest}, Lowest value: ${lowest}`);
