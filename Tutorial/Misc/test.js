f7e7622c7da648143ace86b2bcade679b80461a4
jojo3416 <jojo3416jo@gmail.com>
2026-02-03 21:24:25 +0530

    updated classwork

--- Tutorial/Misc/test.js
+++ Tutorial/Misc/test.js
@@ -1,28 +1,8 @@
-// Contact 1: Loid Forger
-const contact1 = {
-  name: 'Loid Forger',
-  phone: '901-234-5678',
-  email: 'twilight.spy@ostania-mail.com',
-  getInfo: function() {
-    return `Name: ${this.name} | Phone: ${this.phone} | Email: ${this.email}`;
-  }
-};
-
-// Contact 2: Mikasa Ackerman
-const contact2 = {
-  name: 'Mikasa Ackerman',
-  phone: '809-876-5432',
-  email: 'stronger.together@shiganshina.net',
-  getInfo: function() {
-    return `Name: ${this.name} | Phone: ${this.phone} | Email: ${this.email}`;
-  }
-};
-
-function printContactInfo(contact) {
-  // Check if the method exists to avoid the TypeError you saw earlier
-  if (typeof contact.getInfo === 'function') {
-    console.log(contact.getInfo());
-  } else {
-    console.log("Error: This object does not have a valid getInfo method.");
-  }
-}
+var date = "2000-11-14";
+var date2 = new Date(date);
+var month = Number(date2.getMonth());
+var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+console.log(date);
+console.log(months[month]);
+console.log(date2.getFullYear());
+console.log(date2.getDate());
