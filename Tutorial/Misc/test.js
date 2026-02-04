8f8dcb813ba683a9cefc4659bc84c0d7b28517ec
jojo3416 <jojo3416jo@gmail.com>
2026-02-04 20:18:30 +0530

    updated homework

--- Tutorial/Misc/test.js
+++ Tutorial/Misc/test.js
@@ -1,8 +1,35 @@
 
-i = 1;
+const number = Number(prompt("Enter your prediction"));
+let i = 1;
 
-while(i <= 5){
+while(i <=5){
 
-    console.log(i);
-    i++;
+
+
+switch(number){
+    case 1:
+        console.log("You guessed wrong");
+        break;
+    
+    case 2:
+        console.log("You guessed wrong");
+        break;
+
+    case 3:
+        console.log("You guessed wrong");
+        break;
+
+    case 4:
+        console.log("You guessed right");
+        break;
+    
+    case 5:
+        console.log("You guessed wrong");
+        break;
+
+    default:
+        console.log("Enter number between 1 and 5");
+        break;
+}
+i++;
 }
