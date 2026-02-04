f459eb83cf56e31539275791d0747aed03eb15db
jojo3416 <jojo3416jo@gmail.com>
2026-02-04 18:51:17 +0530

    updated classwork

--- Tutorial/Misc/test.js
+++ Tutorial/Misc/test.js
@@ -1,8 +1,8 @@
-var date = "2000-11-14";
-var date2 = new Date(date);
-var month = Number(date2.getMonth());
-var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-console.log(date);
-console.log(months[month]);
-console.log(date2.getFullYear());
-console.log(date2.getDate());
+
+i = 1;
+
+while(i <= 5){
+
+    console.log(i);
+    i++;
+}
