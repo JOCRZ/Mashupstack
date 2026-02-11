1d4d437ffa9c35b9a4b3abbc9f4499754b762c93
jojo3416 <jojo3416jo@gmail.com>
2026-02-11 15:53:16 +0530

    udated

---
+++ Tutorial/4.JavaScript/Problems/p2.js
@@ -0,0 +1,22 @@
+// 1 JavaScript Program To Print Hello World
+
+console.log("Hello World");
+
+// 2 JavaScript Program to Add Two Numbers
+
+let x = 2;
+let y = 3;
+console.log(x+y);
+
+// 3 JavaScript Program to Find the Square Root
+
+let x2 = 2;
+console.log(x*2);
+
+// 4 JavaScript Program to Calculate the Area of a Triangle
+
+let l = 2;
+let b = 3;
+let h = 4;
+
+console.log("Area = "+ l*b*h);
