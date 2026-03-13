8619ce7cdcbf215e001ae2dcc235f4bce8f34484
jojo3416 <jojo3416jo@gmail.com>
2026-03-14 01:54:24 +0530

    updated

--- Tutorial/6.Java/Misc/Main.java
+++ Tutorial/6.Java/Misc/Main.java
@@ -1,8 +1,35 @@
-public class Main{
 
+import java.util.Scanner;
+
+
+public class Main {
+    
     public static void main(String[] args){
-
-        System.out.println("Hello check");
-
+        
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Enter Your Name: ");
+        String name = input.nextLine();
+        
+        System.out.print("Enter Your Age: ");
+        int age = input.nextInt();
+        
+        System.out.print("What is your GPA: ");
+        double gpa = input.nextDouble();
+        
+        System.out.print("Are you a student? (true/false): ");
+        boolean isStudent = input.nextBoolean();
+        
+        
+        System.out.println(name + " is " + age + " years old" + " and your GPA is " + gpa);
+        
+        if(isStudent){
+            System.out.println("You are enrolled");
+        }
+        else{
+            System.out.println("You are not enrolled");
+        }
+        
+        input.close();
     }
 }
