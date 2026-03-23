848d66e7d8acb0af5ff373945b95d676b30405f8
jojo3416 <jojo3416jo@gmail.com>
2026-03-23 22:00:24 +0530

    updated homework

---
+++ Tutorial/6.Java/Lab39/homework/SmallShop.java
@@ -0,0 +1,18 @@
+package Lab39.homework;
+
+public class SmallShop {
+	
+	public static void main(String[] args) {
+		
+		String item = "Soap";
+		int quantity = 4;
+		Double unit_price = 18.75;
+		
+		System.out.println("Small Shop Bill");
+		System.out.println("________________");
+		System.out.println("Item  *  Quantity  = Price");
+		System.out.printf("%s        %d        %f",item,quantity,unit_price);
+				
+	}
+
+}
