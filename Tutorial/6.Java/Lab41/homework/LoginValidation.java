0eff862f035aa595dc3862964de12175188d2ba8
jojo3416 <jojo3416jo@gmail.com>
2026-03-25 21:23:07 +0530

    updated

---
+++ Tutorial/6.Java/Lab41/homework/LoginValidation.java
@@ -0,0 +1,44 @@
+
+import java.util.Scanner;
+
+public class LoginValidation {
+
+		public static void main(String[] args) {
+			
+			Scanner input = new Scanner(System.in);
+			
+		
+			String apass = "1234";
+			String spass = "1111";
+		
+			
+			System.out.println("User code A for Admin, S for Student and G for Guest");
+			System.out.println("Enter user code: ");
+			String user = input.nextLine();
+			
+			if (user.equals("A") || user.equals("S")) {
+				
+				System.out.println("Enter Password: ");
+				String pass = input.nextLine();
+				
+				if (pass.equals(apass)) {
+					System.out.println("Welcome Admin. Full access granted and role ID is 1.");
+				}
+				else if (pass.equals(spass)) {
+					System.out.println("Welcome Student. Limited access granted and role ID is 2.");
+				}
+				else {
+					System.out.println("Enter correct password");
+				}
+			}
+			else if (user.equals("G")) {
+				System.out.println("Welcome Guest. View only access granted and role ID is 3.");
+			}
+			else {
+				System.out.println("Enter correct code");
+			}
+			
+			input.close();
+		    
+		}
+}
