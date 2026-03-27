60df9790b14a949b29d09b1ef241d56eb8989f58
jojo3416 <jojo3416jo@gmail.com>
2026-03-27 15:12:46 +0530

    updated

---
+++ Tutorial/6.Java/Lab43/classwork/InventoryChecker.java
@@ -0,0 +1,28 @@
+
+public class InventoryChecker {
+     public static void main(String[] args) {
+		 
+		 int arr[] = {20,15,30,0,25};
+		 
+		 for (int i : arr) {
+			 
+			 if (i == 0) {
+				 
+				 System.out.println("Out of Stock");
+				 break;
+			 }
+			 else {
+				 
+				 if (i < 50) {
+					 System.out.println("Low Stock");
+				 }
+				 else if (i >= 50 && i <= 100) {
+					 System.out.println("Moderate Stock");
+				 }
+				 else {
+					 System.out.println("Above Stock");
+				 }
+			 }
+		 }
+	 }
+}
