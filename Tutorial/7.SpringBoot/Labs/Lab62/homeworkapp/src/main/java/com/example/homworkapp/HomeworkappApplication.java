c222e874b3c5f9f02c52d236c1154dea71d240bb
jojo3416 <jojo3416jo@gmail.com>
2026-05-08 21:25:49 +0530

    updated

---
+++ Tutorial/7.SpringBoot/Labs/Lab62/homeworkapp/src/main/java/com/example/homworkapp/HomeworkappApplication.java
@@ -0,0 +1,13 @@
+package com.example.homworkapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworkappApplication.class, args);
+	}
+
+}
---
+++ Tutorial/7.SpringBoot/Labs/Lab62/homeworkapp/src/test/java/com/example/homworkapp/HomeworkappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homworkapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
