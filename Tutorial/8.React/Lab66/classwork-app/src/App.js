0276922ec9c077e58f26211303c11d6fa5e5682d
jojo3416 <jojo3416jo@gmail.com>
2026-05-20 00:17:19 +0530

    updated

--- Tutorial/8.React/Lab66/classwork-app/src/App.js
+++ Tutorial/8.React/Lab66/classwork-app/src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import erenImage from './images/eren.jpg';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    console.log("React app started");
+
+    let name = "Eren Yeager";
+
+    return (
+
+        <div className="container py-5 d-flex justify-content-center">
+
+            <div className="card welcome-card shadow p-4 text-center">
+
+                <h1 className="title mb-4">
+                    Welcome to React Learning, {name}
+                </h1>
+
+                {/* External Image */}
+                <img
+                    src="https://i.pinimg.com/736x/5d/b1/8d/5db18dcfb41ec7e1066f074200a643dd.jpg"
+                    alt="External"
+                    className="img-fluid rounded mb-3 card-image"
+                />
+
+                {/* Internal Image */}
+                <img
+                    src={erenImage}
+                    alt="Internal"
+                    className="img-fluid rounded mb-3 card-image"
+                />
+
+                <p className="description fw-bold">
+                    This is your first card with images and styles!
+                </p>
+
+            </div>
+
+        </div>
+    );
 }
 
 export default App;
